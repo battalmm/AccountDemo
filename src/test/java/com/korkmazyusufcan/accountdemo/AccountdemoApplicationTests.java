016bf03a461eef00b4ce5234e278127a98fa90a5
Yusuf Can Korkmaz <korkmasyusufcan@gmail.com>
2023-09-07 14:20:06 +0300

    initial commit

---
+++ src/test/java/com/korkmazyusufcan/accountdemo/AccountdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korkmazyusufcan.accountdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
